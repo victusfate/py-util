7457fb1ae604482beaf8f9b3eff449bee08542ae
Mark Essel <messel@gmail.com>
2011-05-02 11:26:22 -0700

    added a merge, debug tonight

--- sort.py
+++ sort.py
@@ -17,6 +17,25 @@ def qsort(list):
 
     return qsort(less) + equal + qsort(greater)
 
+def merge(left,right):
+	res=[]
+	il=0,ir=0
+	while (il < len(left)) or (ir < len(right)):
+		if (il < len(left)) and (ir < len(right)):
+			if left[il] <= right[ir]:
+				res.append(left[il])
+				il++
+			else:
+				res.append(right[ir])
+				ir++
+		elif il < len(left):
+			res.append(left[range(il,len(left)-1] # gotta check on this
+			il = len(left)
+		elif ir < len(right):
+			res.append(right[range(ir,len(right)-1]
+			ir = len(right)
+	return res
+
 def merge_sort(list):
 	if len(list) <= 1:
 		return list
@@ -26,11 +45,11 @@ def merge_sort(list):
 		mid = list[imid]
 		left, right = [], []
 		for i in range(0,l):
-			if i < imid:
+			if i <= imid:
 				left.append(list[i])
 			elif i > imid:
 				right.append(list[i])
-	return merge_sort(left) + [mid] + merge_sort(right)
+	return merge(merge_sort(left) + merge_sort(right))
 
 if __name__ == "__main__":
 	d = [0,11.6,2,4.3,5.5,9,112,8,77.4,4]
